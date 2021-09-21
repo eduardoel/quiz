64671f8daac9608043a63710f8e315f5a65bb195
Eduardo Elly <eduardo_elly@hotmail.com>
2021-09-21 10:59:20 -0300

    Answer animation

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -12,38 +12,35 @@ interface AnswerProps {
 export default function Answer(props: AnswerProps) {
 
     const answer = props.value
+    const answerRevealed = answer.revealed ? styles.answerRevealed : ''
 
     return (
         <div className={styles.answer}
             onClick={() => props.onResponse(props.indice)}>
-            <div className={styles.answerContent}>
-                {!answer.revealed ? (
-                    <div className={styles.answerFront}>
-                        <div className={styles.letter}
-                            style={{ backgroundColor: props.letterBackgroundColor }}>
-                            {props.letter}
-                        </div>
-                        <div className={styles.value}>
-                            {answer.value}
-                        </div>
+            <div className={`${answerRevealed} ${styles.answerContent}`}>
+                <div className={styles.answerFront}>
+                    <div className={styles.letter}
+                        style={{ backgroundColor: props.letterBackgroundColor }}>
+                        {props.letter}
                     </div>
-                ) : (
-                    <div className={styles.answerBack}>
-                        {answer.correct ? (
-                            <div className={styles.certain}>
-                                <div>The right answer is...</div>
-                                <div className={styles.value}>{answer.value}</div>
-                            </div>
-                        ) : (
-                            <div className={styles.wrong}>
-                                <div>The answer is wrong...</div>
-                                <div className={styles.value}>{answer.value}</div>
-                            </div>
-                        )}
+                    <div className={styles.value}>
+                        {answer.value}
                     </div>
-                )}
+                </div>
 
-
+                <div className={styles.answerBack}>
+                    {answer.correct ? (
+                        <div className={styles.certain}>
+                            <div>The right answer is...</div>
+                            <div className={styles.value}>{answer.value}</div>
+                        </div>
+                    ) : (
+                        <div className={styles.wrong}>
+                            <div>The answer is wrong...</div>
+                            <div className={styles.value}>{answer.value}</div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
